17c204885f3d3056e3bdc1030ca976f8b15dc55d
FranckBisschop <bisschopfranck@gmail.com>
2022-03-28 18:33:25 +0200

    suppression des commentaires dans le routeur.js et index.js

--- app/router.js
+++ app/router.js
@@ -1,11 +1,8 @@
-// Je require express
 const express = require('express');
-// Je lance express
 const router = express.Router();
 
 router.get('/',(req, res) => {
     res.send('Hello World')
 });
 
-// J'exporte mon module
 module.exports = router;
