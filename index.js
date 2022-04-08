a10e9b4166c222fbff584beb6f50583205824d3e
FranckBisschop <bisschopfranck@gmail.com>
2022-04-08 18:17:56 +0200

    modification du fichier index.js et suppression du fichier student.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
-const student = require('./student.js');
 
 const app = express();
 
@@ -18,34 +16,7 @@ const dbName = 'monProjetMongo';
 
 MongoClient.connect(url, function(err, client) {
   console.log("Connecté à MongoDB");
-  const db = client.db(dbName);
-  client.close();
 });
-
-
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("connecté à Mongoose")
-});
-
-
-const createStudent = async studentData => {
-    const student = await Student.create(studentData)
-    return student
-   }
-
-   const findStudent = async firstName => {
-    const student = await Student.findOne({firstName})
-    return student
-   }
-   
-   const findStudents = async firstName => {
-    const student = await Student.find({})
-    return student
-   }
 
 app.use(router);
 
