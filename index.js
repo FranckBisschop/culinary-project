10115ac9ce41b2364e9fd83df2bcde7d8d003b22
FranckBisschop <bisschopfranck@gmail.com>
2022-05-27 17:41:16 +0200

    Création de la méthode insertOne

--- index.js
+++ index.js
@@ -14,17 +14,38 @@ const PORT = process.env.PORT || 4000
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'monProjetMongo';
+let db
 
 MongoClient.connect(url, function(err, client) {
   console.log("Connecté à MongoDB");
+  db = client.db(dbName);
 });
 
 app.use(router);
 app.use(morgan('combined'));
 app.use(express.json());
 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("insertDB");
+    const recipe = database.collection("Recipe");
+    const doc = {
+      "name":"Boeuf Bourguignon",
+      "servings":6,
+      "ingredients":[{"name":"Viande de boeuf bourguignon","unit":"gram",
+      "quantity":800}],
+      "steps":[{"order":1,"instructions":"verser le boeuf dans une cocotte avec le beurre fondu"}]
+    }
+    const result = await recipe.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.post('/recipe', (req,res) => {
-  recipe.push(req.body);
   res.status(201).json({status:"sucess"})
 });
 
